d3e1174d4c13bd23c1c773b171b8d55e2730b0a3
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-07 18:31:16 -0500

    Got Login wired up and rendering.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.css';
+
+import Login from './login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Welcome to Auth-II!</h1>
+        <header>
+          <nav>
+            <NavLink to='/login'>Log-In</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Route path='/login' component={Login} />
+        </main>
       </div>
     );
   }
