d73849b46cf90efdff8097406c3b55d9ff431b8f
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-07 19:23:34 -0500

    Wired up requiresAuth and added logout functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.css';
 
 import Login from './login/Login';
+import Users from './users/Users';
 
 class App extends Component {
   render() {
@@ -12,14 +13,21 @@ class App extends Component {
         <header>
           <nav>
             <NavLink to='/login'>Log-In</NavLink>
+            <NavLink to='/users'>Users</NavLink>
+            <button onClick={this.logout}>Log-Out</button>
           </nav>
         </header>
         <main>
           <Route path='/login' component={Login} />
+          <Route path='/users' component={Users} />
         </main>
       </div>
     );
   }
+  logout = () => {
+    localStorage.removeItem('jwt');
+    this.props.history.push('/login');
+  }
 }
 
-export default App;
+export default withRouter(App);
